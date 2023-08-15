80829c60e9f82c78dd65700358992f6a071ea95b
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-08-15 18:26:47 -0500

    "create function to delete room"

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -39,6 +39,14 @@ class RoomsController < ApplicationController
     redirect_to user_property_room_path(@user, @property, @room)
   end 
 
+  def destroy 
+    @user = current_user
+    @property = @user.properties.find(params[:property_id])
+    @room = @property.rooms.find(params[:id])
+    @room.destroy
+    redirect_to user_property_rooms_path(@user, @property)
+  end 
+
   private
   def room_params
     params.require(:room).permit(:room_type, :price, :smoking, :count, :room_number)
--- spec/features/room/destroy_spec.rb
+++ spec/features/room/destroy_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Delete Property", type: :feature do 
-  describe "when i visit the properties show page" do 
+RSpec.describe "Delete Room", type: :feature do 
+  describe "when i visit the rooms show page" do 
     it 'has a button to delete the property' do 
       user = User.create(username: "karanm645", password: "123")
       visit "/users/#{user.id}"
@@ -10,15 +10,15 @@ RSpec.describe "Delete Property", type: :feature do
       click_button "Log In"
 
       property1 = user.properties.create!(name: "days inn", street: "111", city: "ABQ", state: "NM", zip_code: 77963, phone_number: 7206335555)
+      room1 = property1.rooms.create!(room_type: "NK1", price: 16.99, smoking: true, count: 22, room_number: 114)
 
-      visit user_property_path(user, property1)
+      visit user_property_room_path(user, property1, room1)
 
-      expect(page).to have_content("Days inn")
+      expect(page).to have_content("NK1")
 
       click_button "Delete"
 
-      visit user_property_rooms_path(user, property1)
-      expect(page).to_not have_content("days inn")
+      expect(page).to_not have_content("NK1")
     end 
   end 
 end 
