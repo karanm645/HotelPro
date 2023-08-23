d2f45716c7fc2a53a9fca526688bed554c17562a
Karan Mehta <79548116+karanm645@users.noreply.github.com>
2023-08-23 16:46:23 -0500

    "created a one to many relations (guests to properties)"

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_231828) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_160331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,6 +28,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_231828) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "property_id"
+    t.index ["property_id"], name: "index_guests_on_property_id"
   end
 
   create_table "properties", force: :cascade do |t|
@@ -62,6 +64,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_231828) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "guests", "properties"
   add_foreign_key "properties", "users"
   add_foreign_key "rooms", "properties"
 end
